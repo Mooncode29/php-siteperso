5c625bd2dc66b33f8db721da79a7c3893c58278c
Mooncode29 <irene.ravelonarivo@gmail.com>
2017-01-08 22:31:28 +0100

    etape 5 ok : création formulaire de contact

--- pages/contact.php
+++ pages/contact.php
@@ -1,1 +1,17 @@
-<p>Me contacter : jane.doe@email.com</p>
+<!-- <p>Me contacter : jane.doe@email.com</p> -->
+
+<form action="save.php" method="post">
+	<div>
+		<label for="name">Nom</label>
+	</div>
+	<div>
+		<input type="text" name="name" id="name">
+	</div>	
+	<div>
+		<label for="message">Message</label>
+	</div>
+	<div>
+		<textarea name="message" id="message" cols="30" rows="10">bonjour voici mon message</textarea>
+	</div>
+	<button>envoyer message</button>
+</form>
